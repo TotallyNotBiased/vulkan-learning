d787b510b959b66645c2ffee32d42e7fc703678a
TotallyNotBiased <hi@jerromeyong.com>
2026-08-02 21:29:54 +1000

    chore: initial commit with rust vulkan flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "rust flake template";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      libPath = with pkgs; lib.makeLibraryPath [
+        libGL
+        libxkbcommon
+        wayland
+      ];
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+
+          vulkan-tools
+          vulkan-loader
+          vulkan-validation-layers
+        ];
+        shellHook = "echo 'rust env loaded'";
+        LD_LIBRARY_PATH = libPath;
+      };
+    };
+}
