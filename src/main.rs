080b3a347d4bd4d9b4ea600baa38bc6ee40cf233
TotallyNotBiased <hi@jerromeyong.com>
2026-08-04 23:02:54 +1000

    feat: playing with buffers

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,19 @@ use vulkano::VulkanLibrary;
 use vulkano::instance::{Instance, InstanceCreateFlags, InstanceCreateInfo};
 use vulkano::device::QueueFlags;
 use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo};
-use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferReadGuard, BufferUsage};
 use vulkano::memory::allocator::StandardMemoryAllocator;
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter};
 
+// #[derive(BufferContents)]
+// #[repr(C)]
+// struct MyStruct {
+//     a: u32,
+//     b: u32,
+// }
 
 fn main() {
+    // Load library and create instance
     let library = VulkanLibrary::new().expect("No local Vulkan library/DLL found");
     let instance = Instance::new(
         library,
@@ -19,6 +26,7 @@ fn main() {
         },
     ).expect("Failed to create instance");
 
+    // Identify physical device
     let physical_device = instance
         .enumerate_physical_devices()
         .expect("Could not enumerate devices")
@@ -29,6 +37,7 @@ fn main() {
         println!("Found a queue family with {:?} queue(s)", family.queue_count);
     }
 
+    // Identify device queues
     let queue_family_index = physical_device
         .queue_family_properties()
         .iter()
@@ -37,6 +46,7 @@ fn main() {
         })
         .expect("Couldn't find a graphical queue family") as u32;
 
+    // Create device
     let (device, mut queues) = Device::new(
         physical_device,
         DeviceCreateInfo { 
@@ -50,23 +60,37 @@ fn main() {
     .expect("Failed to create device");
 
     let queue = queues.next().unwrap();
-
     let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
-    let data: i32 = 12;
-    let buffer = Buffer::from_data(
+    // Now we have a virtual device, a queue, and a memory allocator. Let's make a buffer.
+    
+    let iter = (0..128).map(|_| 5u8); // an iterator that resolves
+                                                                      // to a buffer of value 5 in u8s
+                                                                      // 128 times
+    let buffer = Buffer::from_iter( // resolve to buffer
         memory_allocator.clone(),
-        BufferCreateInfo { 
+        BufferCreateInfo {
             usage: BufferUsage::UNIFORM_BUFFER,
             ..Default::default()
-            },
-            AllocationCreateInfo { memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
                 | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
             ..Default::default()
         },
-        data,
+        iter,
     )
-    .expect("failed to create buffer");
+    .unwrap();
 
+    {
+        let mut content = buffer.write().unwrap();
+        content[12] = 83;
+        content[7] = 3;
+    } // lifetimes c:
+
+    let guard = buffer.read().unwrap();
+    let inner_ref: &[u8] = &*guard;
+
+    println!("Thingy: {:?}", inner_ref);
 
 }
